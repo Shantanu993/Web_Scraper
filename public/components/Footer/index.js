01a7587c515eecfe5e5658df49f4866ca20c6bef
Shantanu993 <shantanugahtori4@gmail.com>
2023-10-25 22:21:44 +0530

    added footer

---
+++ public/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      <p>Copyright &#169; 2023 Shantanu Gahtori. All Rights Reserved.</p>
+    </footer>
+  )
+}
+
+export default footer
